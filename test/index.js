9d5353859d107b2aca65b5901eb87e61ccea3dde
Fredrik Forsmo <frozzare@users.noreply.github.com>
2016-07-29 08:23:17 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -4,8 +4,8 @@ describe('json2html(object)', function () {
   it ('should return html', function () {
     var html = json2html({'hello': 'world'});
     html.should.equal('{\n\
- <span class="string">"hello"</span>: <span class="string">"world"</span>\n\
-}');
+      <span class="string">"hello"</span>: <span class="string">"world"</span>\n\
+    }');
   });
 });
 
