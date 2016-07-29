07c45bf5e686855fd7ccab0c99d58effef3e744c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-07-29 09:09:35 +0200

    Fix test

--- test/index.js
+++ test/index.js
@@ -5,8 +5,8 @@ describe('json2html(object)', function () {
   it ('should return html', function () {
     var html = json2html({'hello': 'world'});
     html.should.equal('{\n\
-      <span class="string">"hello"</span>: <span class="string">"world"</span>\n\
-    }');
+  <span class="string key">"hello"</span>: <span class="string value">"world"</span>\n\
+}');
   });
 });
 
